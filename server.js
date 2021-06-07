74aee6d87f0cd135332b22d0468034988731226e
Patrick Lago <Patrick@Dinas-MacBook-Air.local>
2021-06-06 19:09:22 -0700

    succesfully started app, need to add additional functions

--- server.js
+++ server.js
@@ -2,13 +2,60 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
-const connection = mysql.createConnection(
+const db = mysql.createConnection(
     {
       host: 'localhost',
-      port: 3001,
       user: 'root',
       password: 'lagoland',
-      database: 'election'
+      database: 'employees'
     },
     console.log('Connected to the election database.')
 );
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log('Welcome to your employee manager!');
+  startPrompt();
+});
+
+function startPrompt() {
+  inquirer.prompt({
+    message: "Please select an option to get started:",
+    type: "list",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+      "Exit"
+    ], 
+    name: "choice"
+  }).then(answers => {
+    switch (answers.choice) {
+      case "View all deparments":
+        viewDepartments()
+        break;
+      case "View all roles":
+        viewRoles()
+        break;
+      case "View all employees":
+        viewEmployees()
+        break;
+      case "Add a department":
+        addDepartment()
+        break;
+      case "Add a role":
+        addRole()
+        break;
+      case "Add an employee":
+        addEmployee()
+        break;
+      case "Update an employee role":
+        updateRole()
+        break;
+    };
+  });
+};
